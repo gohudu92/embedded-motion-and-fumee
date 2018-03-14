93d404db104bf25c37559f8e0b3acc135a33cb20
Jaygem <youness.kafia@devinci.fr>
2018-03-14 14:51:37 +0100

    Added a folder that holds the server in python

---
+++ Server Embedded System/Server Setup.py
@@ -0,0 +1,38 @@
+import pip
+import os
+import subprocess as sub
+
+
+sub.call(["sudo", "apt-get", "install", "python3-flask"])
+
+from Flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+  def HelloWorld():
+      return 'Hello world'
+
+@app.route('/index')
+  def index():
+      return render_template('index.html')
+
+@app.route('/video')
+  def videoLive():
+      return render_template('video.html')
+
+@app.route('/Imageslog')
+  def imageslog():
+      return render_template('ImagesLog.html')
+
+
+
+  if __name__ == '__main__':
+      app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
+
+
