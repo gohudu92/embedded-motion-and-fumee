6a141254414d1b44e21e5e4414c25e58723f6663
MadScientistHK <jeremy.martin12@hotmail.fr>
2018-03-19 18:32:58 +0100

    Add files via upload

--- Server Embedded System/reco.py
+++ Server Embedded System/reco.py
@@ -1,28 +1,6 @@
-from threading import *
-import time
-from time import sleep
-done = 0
-
-def dodo():
-    global done
-    while done!=1:
-        time.sleep(1)
-        print("bonobo")
-        
-def startThread(val):
-    global done
-    thread = Thread(target = dodo)
-    thread.start()
-    done = val
-
-def killThread(val):
-    global done
-    done = val
-    
-
-"""
 import cv2
 import time
+import RPi.GPIO as gpio
 import os
 import email
 import smtplib
@@ -31,15 +9,41 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import time
+from time import sleep
+done = 0
 
+"""
+Démarre le thread du detecteur
+"""
+def startThread(val):
+    global done
+    thread = threading.Thread(target=reco,args=())
+    thread.start()
+    done = val
+
+"""
+Arrete le thread du detecteur 
+"""
+def killThread(val):
+    global done
+    done = val
+    
+"""
+Creer un logBook si il n'existe pas et ajoute les logs dedans
+"""
 def logBook(stamp):
     log = open("logBook.txt", "a")
     log.write("[ALERTE] Un intru a été détecté le : "+stamp+" \n")
     log.close()
 
+"""
+Envoie un mail avec la photo de l'intru détecté
+"""
 def sendMail(stamp):
-    fromadr = "jeremy.martin12@hotmail.fr"
-    toadr = "jeremy.martin@devinci.fr"
+    global message
+    fromadr = "penelopesecurecam@hotmail.com"
+    toadr = "jeremy.martin@devinci.fr,youness.kafia@devinci.fr,hugo.pierre@devinci.fr,guillaume.gouget@devinci.fr"
     msg = MIMEMultipart()
     msg['From'] = fromadr
     msg['To'] = toadr
@@ -57,48 +61,55 @@ def sendMail(stamp):
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(fromadr, "Vahiteo2005")
+    server.login(fromadr, "Penelope")
     text = msg.as_string()
     server.sendmail(fromadr, toadr, text)
     os.remove('/home/pi/Documents/'+stamp+'.jpg')
     server.quit()
+    reco()
 
-cascPath = 'xml/upper.xml'
-bodyCascade = cv2.CascadeClassifier(cascPath)
-video_capture = cv2.VideoCapture(0)
-i=0
-count = 0
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read() 
-    if ret == True:
-        stamp = time.strftime("%c")
-        thread = threading.Thread(target=sendMail,args=(stamp,))
-        sframe=cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        gray = cv2.cvtColor(sframe, cv2.COLOR_BGR2GRAY)
-        bodies = bodyCascade.detectMultiScale(gray,1.2,2)
-        
-        for (x, y, w, h) in bodies:
-            x*=4
-            y*=4
-            h*=4
-            w*=4
-            #cv2.rectangle(frame,(x,y),(x+w,y+h),(102, 102, 0), 2)
-            crop_body = cv2.flip(frame,1)
-            print(count)
-            if(len(bodies)>0):
-                if(count > 30):
-                    cv2.imwrite('/home/pi/Documents/'+stamp+'.jpg',crop_body)
-                    logBook(stamp)
-                    if(thread.is_alive() == False):
-                        thread.start()
-                        print("Envoyé")
-                    count = 0
-                count=count+1
-        cv2.imshow('Video', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break   
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
 """
+Détecteur de partie haute de corps humain
+"""
+def reco():
+    global done
+    cascPath = 'xml/upper.xml'
+    bodyCascade = cv2.CascadeClassifier(cascPath)
+    video_capture = cv2.VideoCapture(0)
+    i=0
+    count = 0
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read() 
+        if ret == True:
+            stamp = time.strftime("%c")
+            mail = threading.Thread(target=sendMail,args=(stamp,))
+            sframe=cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            gray = cv2.cvtColor(sframe, cv2.COLOR_BGR2GRAY)
+            bodies = bodyCascade.detectMultiScale(gray,1.2,2)
+            
+            for (x, y, w, h) in bodies:
+                x*=4
+                y*=4
+                h*=4
+                w*=4
+                #cv2.rectangle(frame,(x,y),(x+w,y+h),(102, 102, 0), 2)
+                crop_body = cv2.flip(frame,1)
+                print(count)
+                if(len(bodies)>0):
+                    if(count > 20):
+                        cv2.imwrite('/home/pi/Documents/'+stamp+'.jpg',crop_body)
+                        logBook(stamp)
+                        if(mail.is_alive() == False):
+                            mail.start()
+                            print("Envoyé n° "+str(count))
+                        count = 0
+                    count=count+1
+            cv2.imshow('Video', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            if done == 1 :
+                break
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
